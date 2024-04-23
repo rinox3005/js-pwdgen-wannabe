62d2a88633ea15f7504e9a90758514f63337b905
rinox3005 <peppe.rinox@gmail.com>
2024-04-23 14:58:46 +0200

    [UPD] added calculation for output variable new value and showed it in HTML

--- pswdgen.js
+++ pswdgen.js
@@ -6,8 +6,17 @@ let outputText = document.getElementById('password');
 //imposto una variabile che racchiude in se il contenuto html dell'elemento con id password 
 let output = outputText.innerHTML;
 
+//richiesta nome utente e salvataggio in una variabile
 const userName = prompt('Enter your name');
 
+//richiesta cognome utente e salvataggio in una variabile
 const userLastName = prompt('Enter your last name');
 
+//richiesta colore preferito utente e salvataggio in una variabile
 const userFavColor = prompt('Enter your favourite color');
+
+//calcolo il nuovo valore della variabile output
+output = `${output}${userName}${userLastName}${userFavColor}12`;
+
+//mostro il valore della variabile output nel documento HTML
+outputText.innerHTML = output;
