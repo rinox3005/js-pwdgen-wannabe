f664e426c9ebd2c7d6ce7e099583a80d9cf25496
rinox3005 <peppe.rinox@gmail.com>
2024-04-23 14:48:07 +0200

    [UPD] added variable to get p html content and added prompts to enter data

--- pswdgen.js
+++ pswdgen.js
@@ -1,2 +1,13 @@
 'use strict';
 
+//assegno ad outputText il valore dell'elemento con id password
+let outputText = document.getElementById('password');
+
+//imposto una variabile che racchiude in se il contenuto html dell'elemento con id password 
+let output = outputText.innerHTML;
+
+const userName = prompt('Enter your name');
+
+const userLastName = prompt('Enter your last name');
+
+const userFavColor = prompt('Enter your favourite color');
